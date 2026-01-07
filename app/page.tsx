32f6b36901ae3cb0cfb6dec9b010cddf8cedd43c
Tojonirina RANDRIAMAMPIONONA <t.randriamampionona.ext@oppycx.com>
2026-01-07 18:10:28 +0100

    UI: Decrease marging top

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SearchForm from "@/components/search-form";
 export default function Home() {
   return (
     <main className="flex md:items-center justify-center h-full w-full">
-      <div className="max-w-3xl mx-auto p-6 mt-24 md:mt-0 text-center">
+      <div className="max-w-3xl mx-auto p-6 mt-16 md:mt-0 text-center">
         <Heading />
 
         <p className="text-gray-700 text-lg max-w-xl mx-auto">

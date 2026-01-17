1bbc082a25cf308df95f28b01f0cdf38d5c26375
Tojonirina RANDRIAMAMPIONONA <t.randriamampionona.ext@oppycx.com>
2026-01-17 17:42:27 +0100

    FIX: Trigger a push

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen`}
+          className={`${geistSans.variable} ${geistMono.variable} ok antialiased h-screen`}
         >
           <ThemeProvider
             attribute="class"
